ba35120602fdd26d0ee350552ac943a5f1a20236
Moskalev Mikhail <mmv-ru@yandex.ru>
2012-05-20 17:32:21 +0400

    Attempt to work with BeautifulSoup v3

---
+++ datashield2keepass.py
@@ -0,0 +1,29 @@
+﻿#!pyton
+# -*- coding~: utf8 -*-
+# Tested on Python Release 2.5 Win32
+
+version = "0.0.1"
+
+# BeautifulSoup 3
+# http://www.crummy.com/software/BeautifulSoup/bs3/documentation.html
+from BeautifulSoup import BeautifulStoneSoup
+#import string
+#import sys
+#import os
+# http://docs.python.org/library/quopri.html
+import quopri
+
+def main():
+  soup=openfile(u"Datashield Export_Example.xml")
+#  print(soup.findAll('category'))
+#  print(soup.findAll('template'))
+  print(soup.findAll('record'))
+  pass
+
+def openfile(Filename):
+#  with open(Filename) as file:
+    file = open(Filename)
+    return BeautifulStoneSoup(file)
+  
+if __name__ == '__main__':
+    main()
---
+++ tests.py
@@ -0,0 +1,10 @@
+﻿#!pyton
+# -*- coding~: utf8 -*-
+# Tested on Python Release 2.5 Win32
+
+import datashield2keepass
+
+def test():
+  soup=datashield2keepass.openfile(u"Datashield Export_Example.xml")
+  print("test")
+  print(soup)
