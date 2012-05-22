21cdd936282716c2fb5b2026cff180b0f7d08981
Moskalev Mikhail <mmv-ru@yandex.ru>
2012-05-23 00:24:27 +0400

    work with cp1251 and urlencoded. Begin decoding templates

--- datashield2keepass.py
+++ datashield2keepass.py
@@ -12,18 +12,46 @@ from BeautifulSoup import BeautifulStoneSoup
 #import os
 # http://docs.python.org/library/quopri.html
 import quopri
+import urllib
 
 def main():
-  soup=openfile(u"Datashield Export_Example.xml")
-#  print(soup.findAll('category'))
-#  print(soup.findAll('template'))
-  print(soup.findAll('record'))
-  pass
+    soup=openfile(u"Datashield Export_Example.xml")
+#    print(soup.findAll('category'))
+#    print(soup.findAll('template'))
+#    print(soup.findAll('record'))
+    print(u"тест")
+    print(soup.originalencoding)
+    cat = ParseCategories(soup)
+    print 9, cat['9']['name']
+    templates = ParseTemplates(soup)
+    print templates['32767']
+    pass
 
+def ParseCategories(soup):
+    """Parse categories"""
+    return dict(map(lambda cat: (cat['id'], {'id': cat['id'], 'rid': cat['rid'],
+                                             'name': cat.string.strip() }),
+                    soup.findAll('category')
+                   )
+               )
+
+def ParseTemplates(soup):
+    """Parse templates"""
+    return dict(map(lambda cat: (cat['id'], {'id': cat['id'], 'name': cat['name'], 'flags': cat['flags'],
+                                        'fields': ' '.strip() }),
+             soup.findAll('template')))
+
+			 
 def openfile(Filename):
-#  with open(Filename) as file:
-    file = open(Filename)
-    return BeautifulStoneSoup(file)
+    """Open file as BeautifulStoneSoup"""
+#    with open(Filename) as file: #not supported in python before 2.6
+    try:
+        file = open(Filename, 'rb')
+        soup = BeautifulStoneSoup( urllib.unquote( file.read().encode('cp1251')
+		                                         ))
+    finally:
+        file.close
+    return soup
   
 if __name__ == '__main__':
     main()
